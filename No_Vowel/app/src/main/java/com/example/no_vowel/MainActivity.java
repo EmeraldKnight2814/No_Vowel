bc10b2b365a897df8e3ef480258d07b8748288de
Daniel J Crowley <dcrow2814@gmail.com>
2023-01-26 09:02:02 -0600

    Added base functionality

---
+++ No_Vowel/app/src/main/java/com/example/no_vowel/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.no_vowel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void checkForInput(View trigger){
+        TextView displayTV = (TextView)findViewById(R.id.displayTV);
+        TextView phraseET = (TextView)findViewById(R.id.phraseET);
+        System.out.println(trigger);
+        System.out.println("Wow. Mutate Button Pressed. Checking for input now.");
+
+        if(phraseET.getText() != ""){
+            System.out.println("Wow! Input found!");
+            String string_to_change = String.valueOf(phraseET.getText());
+            String changed_string;
+            changed_string = mutate(string_to_change);
+            System.out.println(changed_string);
+            displayTV.setText(changed_string);
+        }
+        else{
+            System.out.println("Wow! No input found!");
+            String str = "Wow! No input found!";
+            displayTV.setText(str);
+        }
+
+    }
+
+    public String mutate(String string_in){
+        //For loop to check each character for vowel
+        String new_string = "";
+
+        for(int i = 0; i < string_in.length(); i++){
+            if(string_in.charAt(i) == 'a'
+                    || string_in.charAt(i) == 'e'
+                    || string_in.charAt(i) == 'i'
+                    || string_in.charAt(i) == 'o'
+                    || string_in.charAt(i) == 'u'){
+                System.out.println("Vowel");
+            }
+            else{
+                System.out.println("Not a Vowel");
+                new_string += string_in.charAt(i);
+            }
+        }
+
+        return new_string;
+    }
+}
